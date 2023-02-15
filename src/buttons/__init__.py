25b4df08ca4438d81446e7b3c00f142147a71ef9
Hozen <enzo.durel@gmail.com>
2023-02-15 21:39:11 +0100

    git not ignore packages src py files

---
+++ src/buttons/__init__.py
@@ -0,0 +1,10 @@
+"""!
+@brief Package corresponding to all the functions specific to buttons
+@author Durel Enzo
+@author Mallepeyre Nourrane
+@version 1.0
+"""
+from .button import Button
+from .check_box import CheckBox
+from .input_box import InputBox
+from .menu import Menu
---
+++ src/utils/color.py
@@ -0,0 +1,39 @@
+"""!
+@brief Package of all colors used in the program
+@author Durel Enzo
+@author Mallepeyre Nourrane
+@version 1.0
+"""
+
+# color dictionnary (basic color) #
+dic = dict()
+dic["white"] = (255, 255, 255)
+dic["black"] = (0, 0, 0)
+dic["red"] = (255, 0, 0)
+dic["green"] = (0, 255, 0)
+
+# Button special colors #
+INACTIVE_BUTTON_COLOR = (46, 107, 81)
+ACTIVE_BUTTON_COLOR = (69, 153, 125)
+HOVER_BUTTON_COLOR = (49, 122, 110)
+DISABLE_BUTTON_COLOR = (55, 64, 60)
+
+TEXT_BUTTON_COLOR = dic["white"]
+
+# Input Box special colors #
+INACTIVE_IB_COLOR = (46, 107, 81)
+ACTIVE_IB_COLOR = (186, 191, 119)
+DISABLE_IB_COLOR = (55, 64, 60)
+
+TEXT_IB_COLOR = dic["black"]
+
+# Check Box special colors #
+INACTIVE_CB_COLOR = (46, 107, 81)
+ACTIVE_CB_COLOR = (186, 191, 119)
+DISABLE_CB_COLOR = (55, 64, 60)
+DISABLE_ACTIVE_CB_COLOR = (87, 56, 53)
+
+TEXT_CB_COLOR = dic["black"]
+
+# Menu colors #
+MENU_COLOR = (103, 168, 120)
